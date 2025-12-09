a9ecc7504d3f7831ed4366a66a34a80c2e05b3f6
Anna Makarudze <amakarudze@gmail.com>
2025-12-09 13:50:33 +0100

    Fix e2e tests configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: 'e2e',
+  testMatch: /.*\.e2e\.ts/,
   use: {
     baseURL: 'http://localhost:4321',
     headless: true,
